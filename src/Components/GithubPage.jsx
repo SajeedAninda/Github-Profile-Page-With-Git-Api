15edd83e7fbc3d35965aa0027da166158b35bfdd
SajeedAninda <sajaninda@gmail.com>
2024-08-20 17:59:59 +0600

    Made responsive

--- src/Components/GithubPage.jsx
+++ src/Components/GithubPage.jsx
@@ -11,25 +11,25 @@ const GithubPage = () => {
     return (
         <div>
             <div className='relative upperDiv'>
-                <div>
-                    <img className='w-full' src={headerImage} alt="" />
+                <div className='h-[40vh]'>
+                    <img className='w-full h-full object-cover' src={headerImage} alt="" />
                 </div>
 
-                <div className='searchBox absolute w-[38%] left-1/2 transform -translate-x-1/2 top-6'>
+                <div className='searchBox absolute w-[90%] lg:w-[38%] left-1/2 transform -translate-x-1/2 top-6'>
                     <input className='w-full rounded-xl py-4 pl-[55px] pr-6 text-[#CDD5E0] bg-[#212a3b] placeholder:text-[#4A5567] font-bold outline-none' placeholder='Username' type="text" />
                     <img className='absolute left-4 top-1/2 transform -translate-y-1/2' src={searchIcon} alt="" />
                 </div>
             </div>
 
 
-            <div className='bottomDiv h-screen bg-[#20293A] relative'>
+            <div className='bottomDiv h-fit lg:h-screen bg-[#20293A] relative'>
                 <div className='w-[90%] mx-auto '>
                     <div className="profileInfo flex items-center justify-start">
                         <div className='absolute -top-12'>
                             <img className='w-[126px] rounded-xl border-8 border-[#20293A]' src={profileImg} alt="" />
                         </div>
 
-                        <div className='flex justify-center items-center gap-5 ml-36 mt-4'>
+                        <div className='flex flex-col lg:flex-row items-start lg:items-center gap-5 ml-36 mt-4'>
                             <div className='bg-[#111729] px-5 py-4 rounded-xl flex justify-center items-center'>
                                 <div className='pr-3 border-r border-[#4A5567]'>
                                     <p className='text-[#4A5567] text-[16px] font-semibold'>
@@ -85,7 +85,7 @@ const GithubPage = () => {
                         </h3>
                     </div>
 
-                    <div className="repos grid grid-cols-2 gap-6 mt-6">
+                    <div className="repos grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6">
                         <div className="repoCard h-[167px] bg-gradient-to-r from-[#111729] to-[#1D1B48] rounded-xl p-4">
                             <h3 className='text-[20px] font-semibold text-[#CDD5E0]'>
                                 Advice Generator App
